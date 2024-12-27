8e06975666de13f11651eecb65fb8fe0dd1c78a4
Dany Blais Benoit <dany.blais.benoit@gmail.com>
2024-12-27 07:29:15 -0500

    Addition of the BetterRest application.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dany Blais Benoit on 2024-12-24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
