6dc602386322896d1b642d336655ee57ad2a3d8b
Dany Blais Benoit <dany.blais.benoit@gmail.com>
2024-12-29 09:37:16 -0500

    Completed WorldScramble project (5).

---
+++ WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Dany Blais Benoit on 2024-12-28.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
