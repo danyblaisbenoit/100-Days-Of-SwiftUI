03849388f21e59174fcbabada7dff4dc9ee817a1
Dany Blais Benoit <dany.blais.benoit@gmail.com>
2025-01-13 17:42:59 -0500

    Addition of CupcakeCorner app (day 49).

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dany Blais Benoit on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let playtime = 12.1
+
+print(playtime.rounded())
+
+if 
